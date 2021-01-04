3056095bba51348081c11e0cb217589c5da76f4e
Brandon Pachuca <bpachuca1@gmail.com>
2021-01-04 13:35:33 -0500

    del

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -42,8 +42,7 @@ module.exports = {
           children: [
             '',
             'rhino-to-scout',
-            'required-files.md',
-            'KPF-compute-plugin'
+            'required-files.md'
           ]
         }
       ]
